afc2e6e608f15417fafc8b0cf103ade26f697d67
Sivaraj-Rajendran <sivarajrajendran1018@gmail.com>
2025-07-04 10:22:39 +0530

    changes

--- src/main/java/Java/Bridge/CalcApp.java
+++ src/main/java/Java/Bridge/CalcApp.java
@@ -1,5 +1,7 @@
 package Java.Bridge;
 
+import Java.Bridge.Assignment.NoReturnTypeMethods;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class CalcApp
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        NoReturnTypeMethods nr= new NoReturnTypeMethods();
+        nr.add(2, 5);
+        System.out.println("Result ==="+nr.getLastResult());
     }
 }
